ad3901f0ee27466815e64976308391c0989d39cc
Rod Henley <rhenley@opinionlab.com>
2016-01-12 09:53:43 -0600

    4: Events in Depth

--- src/components/todo-input.ts
+++ src/components/todo-input.ts
@@ -4,12 +4,12 @@ import {Component} from "angular2/core";
   selector: 'todo-input',
   template: `<div>
   <input type="text" #myInput>
-  <button (click)="onClick(myInput.value)" >Click this button</button>
+  <button (click)="onClick($event, myInput.value)" >Click this button</button>
   </div>`
 })
 
 export class TodoInput{
-  onClick(value) {
-    console.log("I have been clicked with value " + value);
+  onClick(event, value) {
+    console.log("event: "+ event + ", value: " + value);
   }
 }
