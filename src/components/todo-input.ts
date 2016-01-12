8f63eae9ef3ca2f03d12e418ef0ea319673b4c91
Rod Henley <rhenley@opinionlab.com>
2016-01-12 09:50:11 -0600

    3: Using events and refs

--- src/components/todo-input.ts
+++ src/components/todo-input.ts
@@ -2,7 +2,14 @@ import {Component} from "angular2/core";
 
 @Component({
   selector: 'todo-input',
-  template: `<div>my todo input</div>`
+  template: `<div>
+  <input type="text" #myInput>
+  <button (click)="onClick(myInput.value)" >Click this button</button>
+  </div>`
 })
 
-export class TodoInput{}
+export class TodoInput{
+  onClick(value) {
+    console.log("I have been clicked with value " + value);
+  }
+}
